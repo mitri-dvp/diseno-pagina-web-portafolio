837ca41c1d862e94306f7ea365ca9f2dd1e5927d
mitri-dvp <mitri.dvp@gmail.com>
2021-08-10 00:52:55 -0300

    Diseño responsivo finalizado.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 // Elements
 const form = document.querySelector('form')
 const form_output = document.querySelector('.form_output')
+const mobile_button = document.querySelector('.mobile_button')
+const mobile_nav = document.querySelector('.mobile_nav')
 
 // Listeners
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   form_output.classList.remove('dnone')
+})
+
+mobile_button.addEventListener('click', () => {
+  mobile_nav.classList.toggle('active')
 })
